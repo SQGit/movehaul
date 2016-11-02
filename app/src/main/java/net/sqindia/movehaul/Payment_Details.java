d2ecdc65909d901696276e3a62c3cff7c69c3c12
krishna <tulasi@sqindia.net>
2016-11-02 18:23:34 +0530

    krishna

--- app/src/main/java/net/sqindia/movehaul/Payment_Details.java
+++ app/src/main/java/net/sqindia/movehaul/Payment_Details.java
@@ -1,8 +1,11 @@
 package net.sqindia.movehaul;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.rey.material.widget.Button;
 import com.sloop.fonts.FontsManager;
 
 /**
@@ -10,11 +13,24 @@ import com.sloop.fonts.FontsManager;
  */
 
 public class Payment_Details extends Activity {
+    Button btn_continue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_details);
         FontsManager.initFormAssets(this, "fonts/CATAMARAN-REGULAR.TTF");       //initialization
         FontsManager.changeFonts(this);
+
+        btn_continue = (Button) findViewById(R.id.btn_continue);
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Payment_Details.this,Payment_Card_Details.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
