8d5131b1af547879a754d5a97b05a1dcd7c1f7b0
Salman <salman@sqindia.net>
2016-11-02 18:24:44 +0530

    smk commit driverslist_dash

--- app/src/main/java/net/sqindia/movehaul/DashboardNavigation.java
+++ app/src/main/java/net/sqindia/movehaul/DashboardNavigation.java
@@ -1,14 +1,18 @@
 package net.sqindia.movehaul;
 
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,9 +20,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
 import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 import com.rey.material.widget.Button;
@@ -30,7 +37,7 @@ import com.sloop.fonts.FontsManager;
  * Created by SQINDIA on 10/26/2016.
  */
 
-public class DashboardNavigation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class DashboardNavigation extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
 
     Toolbar toolbar;
     DrawerLayout drawer;
@@ -39,8 +46,10 @@ public class DashboardNavigation extends AppCompatActivity implements Navigation
     TextView tv_name, tv_email, tv_myTrips, tv_jobReview, tv_payments, tv_tracking, tv_offers, tv_emergencyContacts;
     EditText et_pickup, et_droplocation;
     TextInputLayout flt_pickup, flt_droplocation;
-    FloatingActionButton flt_icon;
-    com.rey.material.widget.ImageView img_ico;
+    FloatingActionButton fab_truck;
+
+    private boolean serviceWillBeDismissed;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,17 +75,11 @@ public class DashboardNavigation extends AppCompatActivity implements Navigation
         et_droplocation = (EditText) findViewById(R.id.editText_dropLocation);
         flt_pickup = (TextInputLayout) findViewById(R.id.float_pickup);
         flt_droplocation = (TextInputLayout) findViewById(R.id.float_drop);
-
-        flt_icon = (FloatingActionButton) findViewById(R.id.float_icon);
-
-     //   img_ico = (com.rey.material.widget.ImageView) findViewById(R.id.img_ico);
-
+        fab_truck = (FloatingActionButton) findViewById(R.id.float_icon);
 
         Typeface type = Typeface.createFromAsset(getAssets(), "fonts/CATAMARAN-REGULAR.TTF");
         flt_pickup.setTypeface(type);
         flt_droplocation.setTypeface(type);
-
-
 
         final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.app_name, R.string.app_name);
@@ -84,34 +87,97 @@ public class DashboardNavigation extends AppCompatActivity implements Navigation
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-
         SubActionButton.Builder rLSubBuilder = new SubActionButton.Builder(DashboardNavigation.this);
-        ImageView rlIcon1 = new ImageView(DashboardNavigation.this);
+        rLSubBuilder.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        final FrameLayout.LayoutParams tvParams = new FrameLayout.LayoutParams(90,90);
+
+        final ImageView rlIcon1 = new ImageView(DashboardNavigation.this);
         ImageView rlIcon2 = new ImageView(DashboardNavigation.this);
         ImageView rlIcon3 = new ImageView(DashboardNavigation.this);
         ImageView rlIcon4 = new ImageView(DashboardNavigation.this);
         ImageView rlIcon5 = new ImageView(DashboardNavigation.this);
         ImageView rlIcon6 = new ImageView(DashboardNavigation.this);
 
-        rlIcon1.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
-        rlIcon2.setImageDrawable(getResources().getDrawable(R.drawable.truck_icon));
-        rlIcon3.setImageDrawable(getResources().getDrawable(R.drawable.truck_icon));
-        rlIcon4.setImageDrawable(getResources().getDrawable(R.drawable.truck_icon));
-        rlIcon5.setImageDrawable(getResources().getDrawable(R.drawable.truck_icon));
-        rlIcon6.setImageDrawable(getResources().getDrawable(R.drawable.truck_icon));
+        rlIcon1.setBackground(getResources().getDrawable(R.drawable.truck_1));
+        rlIcon1.setLayoutParams(tvParams);
+        rlIcon2.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
+        rlIcon2.setLayoutParams(tvParams);
+        rlIcon3.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
+        rlIcon3.setLayoutParams(tvParams);
+        rlIcon4.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
+        rlIcon4.setLayoutParams(tvParams);
+        rlIcon5.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
+        rlIcon5.setLayoutParams(tvParams);
+        rlIcon6.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
+        rlIcon6.setLayoutParams(tvParams);
 
 
-        // Build the menu with default options: light theme, 90 degrees, 72dp radius.
-        // Set 4 default SubActionButtons
-        final FloatingActionMenu rightLowerMenu = new FloatingActionMenu.Builder(DashboardNavigation.this)
-                .addSubActionView(rLSubBuilder.setContentView(rlIcon1).build())
-                .addSubActionView(rLSubBuilder.setContentView(rlIcon2).build())
-                .addSubActionView(rLSubBuilder.setContentView(rlIcon3).build())
-                .addSubActionView(rLSubBuilder.setContentView(rlIcon4).build())
-                .addSubActionView(rLSubBuilder.setContentView(rlIcon5).build())
-                .addSubActionView(rLSubBuilder.setContentView(rlIcon6).build())
-                .attachTo(flt_icon)
+        SubActionButton.Builder tCSubBuilder = new SubActionButton.Builder(this);
+
+        int blueSubActionButtonContentMargin = getResources().getDimensionPixelSize(R.dimen.blue_sub_action_button_content_margin);
+
+        FrameLayout.LayoutParams blueContentParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        blueContentParams.setMargins(blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin);
+
+        ImageView rlIcons = new ImageView(DashboardNavigation.this);
+
+        rlIcons.setImageDrawable(getResources().getDrawable(R.drawable.truck_1));
+
+        SubActionButton tcSub1 = tCSubBuilder.setContentView(rlIcons, blueContentParams).build();
+
+
+
+        final FloatingActionMenu fam_truck_choose = new FloatingActionMenu.Builder(DashboardNavigation.this)
+                .setRadius(150)
+                .setStartAngle(150)
+                .addSubActionView(rlIcon1)
+                .addSubActionView(rlIcon2)
+                .addSubActionView(rlIcon3)
+                //.addSubActionView(tcSub1, tcSub1.getLayoutParams().width, tcSub1.getLayoutParams().height)
+                .addSubActionView(rlIcon4)
+                .addSubActionView(rlIcon5)
+                .addSubActionView(rlIcon6)
+                .attachTo(fab_truck)
                 .build();
+
+
+
+        fam_truck_choose.setStateChangeListener(new FloatingActionMenu.MenuStateChangeListener() {
+            @Override
+            public void onMenuOpened(FloatingActionMenu menu) {
+
+            }
+
+            @Override
+            public void onMenuClosed(FloatingActionMenu menu) {
+                if(serviceWillBeDismissed) {
+                    serviceWillBeDismissed = false;
+                }
+            }
+        });
+
+        // make the red button terminate the service
+        rlIcon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceWillBeDismissed = true; // the order is important
+              //  fam_truck_choose.close(true);
+                rlIcon1.setBackground(getResources().getDrawable(R.drawable.truck_1_hover));
+            }
+        });
+
+       /* rlIcon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlIcon1.setBackground(getResources().getDrawable(R.drawable.truck_1_hover));
+                rlIcon1.setLayoutParams(tvParams);
+            }
+        });*/
+
+
 
 
 
@@ -167,5 +233,10 @@ public class DashboardNavigation extends AppCompatActivity implements Navigation
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
