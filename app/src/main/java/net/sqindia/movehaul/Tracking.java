20d5d2985d4a154cb2a11e83a79c50391c45a361
krishna <tulasi@sqindia.net>
2016-11-02 13:59:35 +0530

    krishna

--- app/src/main/java/net/sqindia/movehaul/Tracking.java
+++ app/src/main/java/net/sqindia/movehaul/Tracking.java
@@ -2,6 +2,7 @@ package net.sqindia.movehaul;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.sloop.fonts.FontsManager;
 
@@ -9,7 +10,7 @@ import com.sloop.fonts.FontsManager;
  * Created by sqindia on 02-11-2016.
  */
 
-public class Tracking extends Activity {
+public class Tracking extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
