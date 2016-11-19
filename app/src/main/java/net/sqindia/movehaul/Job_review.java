3f1d59f949bb2828d4f12c5554b99923016c2902
krishnaAkirahin <tulasi@sqindia.net>
2016-11-19 13:47:51 +0530

    krishna complete

--- app/src/main/java/net/sqindia/movehaul/Job_review.java
+++ app/src/main/java/net/sqindia/movehaul/Job_review.java
@@ -21,8 +21,9 @@ import java.util.concurrent.TimeUnit;
 
 public class Job_review extends Activity {
     LinearLayout btn_back;
-    Button btn_review;
+    Button btn_continue;
     TextView tv_timer;
+    public Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,41 +32,47 @@ public class Job_review extends Activity {
         FontsManager.changeFonts(this);
 
         btn_back = (LinearLayout) findViewById(R.id.layout_back);
-        btn_review = (Button) findViewById(R.id.btn_review);
+        btn_continue = (Button) findViewById(R.id.btn_continue);
         tv_timer = (TextView) findViewById(R.id.text_Timer);
 
+        btn_continue.setEnabled(false);
 
-        new CountDownTimer(100000, 1000) { // adjust the milli seconds here
+        new CountDownTimer(30000, 1000) { // adjust the milli seconds here
 
             public void onTick(long millisUntilFinished) {
-                if((TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)==4) &&
-                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
-                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))==55)
-                {
 
-                    Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                    // Vibrate for 500 milliseconds
-                    v.vibrate(500);
-                }
                 tv_timer.setText(""+String.format("%d min, %d sec",
                         TimeUnit.MILLISECONDS.toMinutes( millisUntilFinished),
                         TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
                                 TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
 
+                if((TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)==0) &&
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))==10)
+                {
+
+                    Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    // Vibrate for 500 milliseconds
+                    v.vibrate(5000);
+                }
 
             }
 
             public void onFinish() {
+                //onClickVibrate();
                 tv_timer.setText("done!");
+                btn_continue.setEnabled(true);
             }
         }.start();
 
-        btn_review.setOnClickListener(new View.OnClickListener() {
+        btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //vibrator.cancel();
                 Intent i = new Intent(Job_review.this,DriversList.class);
                 startActivity(i);
                 finish();
+
             }
         });
 
@@ -78,18 +85,18 @@ public class Job_review extends Activity {
             }
         });
 
-/*
-        new CountDownTimer(300000, 1000) {
+    }
+    public void onClickVibrate()
+    {
+        //Set the pattern, like vibrate for 300 milliseconds and then stop for 200 ms, then
+        //vibrate for 300 milliseconds and then stop for 500 ms. You can change the pattern and
+        // test the result for better clarity.
+        long pattern[]={0,300,200,300,500};
 
-            public void onTick(long millisUntilFinished) {
-                tv_timer.setText("seconds remaining: " + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                tv_timer.setText("done!");
-            }
-        }.start();
-*/
+        // Start the vibration
+        vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+        //start vibration with repeated count, use -1 if you don't want to repeat the vibration
+        vibrator.vibrate(pattern, 0);
     }
     @Override
     public void onBackPressed() {
