74bbe6459619d1dee191526c153ed7962d644465
mbrsalman5 <salman@sqindia.net>
2017-03-24 18:16:22 +0530

    movehaul update playstore name change

---
+++ app/src/main/java/com/movhaul/customer/Dialog_GoodsType.java
@@ -0,0 +1,309 @@
+package com.movhaul.customer;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.rey.material.widget.TextView;
+
+import java.util.ArrayList;
+
+
+
+/**
+ * Created by Salman on 7/28/2016.
+ */
+
+public class Dialog_GoodsType extends Dialog {
+
+    public Activity activity;
+    TextView tv_header;
+    ImageView img_back;
+    Typeface tf;
+    ArrayList<String> country_lists = new ArrayList<>();
+    ArrayList<String> state_lists = new ArrayList<>();
+    ArrayList<String> zip_lists = new ArrayList<>();
+    ListAdapter adapter1, adapter2, adapter3;
+    View div_view;
+    Context context;
+    Cursor cursor;
+    String query, str_country, str_state, str_zip;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    ArrayList<String> ar_goods_type = new ArrayList<>();
+    ListView lview_cont, lview_state, lview_zip;
+
+
+    /*ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            if (position == 0) {
+                tv_header.setText("Country");
+                img_back.setVisibility(View.GONE);
+                country_lists.clear();
+                adapter1.notifyDataSetChanged();
+
+            } else if (position == 1) {
+                tv_header.setText("State");
+                img_back.setVisibility(View.GONE);
+                state_lists.clear();
+                adapter2.notifyDataSetChanged();
+            } else {
+                tv_header.setText("Zip");
+                img_back.setVisibility(View.GONE);
+                zip_lists.clear();
+                adapter3.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };*/
+   // private ViewPager viewPager;
+   // private int[] layouts;
+    //private MyViewPagerAdapter myViewPagerAdapter;
+
+
+    public Dialog_GoodsType(Activity activity, ArrayList<String> ar_goods) {
+        super(activity);
+        this.activity = activity;
+        this.ar_goods_type = ar_goods;
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.movhaul.customer.R.layout.dialog_region);
+
+        tf = Typeface.createFromAsset(activity.getAssets(), "fonts/lato.ttf");
+
+
+
+
+/*
+        layouts = new int[]{
+                R.layout.register_country,
+       *//*         R.layout.register_state,*//*
+                };*/
+
+
+
+
+
+
+
+
+        lview_cont = (ListView) findViewById(com.movhaul.customer.R.id.lview);
+        adapter1 = new ListAdapter(activity.getApplicationContext(), com.movhaul.customer.R.layout.dialog_vehicle_types, ar_goods_type);
+        lview_cont.setAdapter(adapter1);
+
+
+/*        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });*/
+
+        // viewPager.beginFakeDrag();
+
+       /* img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int current = 0;
+                current = viewPager.getCurrentItem() - 1;
+                // Do something after 5s = 5000ms
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+
+                } else {
+                    // launchHomeScreen();
+                }
+
+            }
+        });*/
+
+
+    }
+
+
+
+  /*  public class MyViewPagerAdapter extends PagerAdapter {
+
+        ListView lview_cont, lview_state, lview_zip;
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+
+            layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            if (position == 0) {
+
+                lview_cont = (ListView) view.findViewById(R.id.lview);
+                adapter1 = new ListAdapter(activity.getApplicationContext(), R.layout.dialog_vehicle_types, country_lists);
+                lview_cont.setAdapter(adapter1);
+
+
+            } else if (position == 1) {
+
+                state_lists.clear();
+
+
+                lview_state = (ListView) view.findViewById(R.id.lview);
+                adapter2 = new ListAdapter(activity.getApplicationContext(), R.layout.dialog_vehicle_types, state_lists);
+                lview_state.setAdapter(adapter2);
+
+
+            } else {
+
+                lview_zip = (ListView) view.findViewById(R.id.lview);
+                adapter3 = new ListAdapter(activity.getApplicationContext(), R.layout.dialog_vehicle_types, zip_lists);
+                lview_zip.setAdapter(adapter3);
+
+            }
+
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }*/
+
+    public class ListAdapter extends ArrayAdapter<String> {
+
+        Context cc;
+        ArrayList<String> data_lists;
+        int resourceid;
+
+        public ListAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
+            super(context, textViewResourceId, objects);
+            this.cc = context;
+            this.data_lists = objects;
+            this.resourceid = textViewResourceId;
+        }
+
+        @Override
+        public int getCount() {
+            return ar_goods_type.size();
+        }
+
+        @Override
+        public View getDropDownView(int posi, View convertView, ViewGroup parent) {
+            return getCustomView(posi, convertView, parent);
+        }
+
+        @Override
+        public View getView(int posi, View convertView, ViewGroup parent) {
+            return getCustomView(posi, convertView, parent);
+        }
+
+
+        public View getCustomView(final int posi, View row, ViewGroup parent) {
+
+            Typeface tf = Typeface.createFromAsset(cc.getAssets(), "fonts/lato.ttf");
+
+            LayoutInflater inflater = (LayoutInflater) cc.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View arow = inflater.inflate(resourceid, parent, false);
+
+            final TextView label = (TextView) arow.findViewById(com.movhaul.customer.R.id.textview_header);
+
+            div_view = arow.findViewById(com.movhaul.customer.R.id.divider_view);
+
+            if(posi == ar_goods_type.size()-1)
+                div_view.setVisibility(View.GONE);
+
+            final ImageView image = (ImageView) arow.findViewById(com.movhaul.customer.R.id.image);
+
+            label.setTypeface(tf);
+
+            label.setText(data_lists.get(posi));
+            Log.e("tag","s: "+data_lists.get(posi));
+
+            arow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    image.setImageDrawable(cc.getResources().getDrawable(com.movhaul.customer.R.mipmap.select_tick));
+
+                    sharedPreferences = PreferenceManager.getDefaultSharedPreferences(cc);
+                    editor = sharedPreferences.edit();
+
+                    editor.putString("goods",label.getText().toString());
+                    editor.commit();
+
+                    final Handler handler = new Handler();
+                    final Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                          dismiss();
+                        }
+                    };
+                    handler.postDelayed(runnable, 500);
+
+
+
+
+                }
+            });
+
+
+
+            return arow;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/movhaul/customer/EmergencyContacts.java
@@ -0,0 +1,196 @@
+package com.movhaul.customer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.TextInputLayout;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+
+import com.rey.material.widget.Button;
+
+import com.rey.material.widget.TextView;
+import com.sloop.fonts.FontsManager;
+
+import org.json.JSONObject;
+
+/**
+ * Created by SQINDIA on 11/1/2016.
+ */
+
+public class EmergencyContacts extends Activity {
+
+    Button btn_submit;
+    TextView tv_hint;
+    TextView tv_emergency_name1,tv_emergency_name2,tv_emergency_no1,tv_emergency_no2,tv_emergency_relation1,tv_emergency_relation2;
+    LinearLayout lt_edit,lt_show;
+    com.rey.material.widget.LinearLayout btn_back;
+    TextInputLayout til_name,til_phone,til_relation;
+
+    String emergencyName1,emergencyName2,emergencyNumber1,emergencyNumber2,emergencyRelation1,emergencyRelation2,service_token,service_id;
+    SharedPreferences sharedPreferences;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.movhaul.customer.R.layout.emergency_contacts);
+
+        FontsManager.initFormAssets(EmergencyContacts.this, "fonts/lato.ttf");       //initialization
+        FontsManager.changeFonts(EmergencyContacts.this);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/lato.ttf");
+
+        btn_submit = (Button) findViewById(com.movhaul.customer.R.id.button_submit);
+        tv_hint = (TextView) findViewById(com.movhaul.customer.R.id.textview_hint);
+        tv_emergency_name1 = (TextView) findViewById(com.movhaul.customer.R.id.textview_emergency_name1);
+        tv_emergency_name2 = (TextView) findViewById(com.movhaul.customer.R.id.textview_emergency_name2);
+        tv_emergency_no1 = (TextView) findViewById(com.movhaul.customer.R.id.textview_emergency_number1);
+        tv_emergency_no2 = (TextView) findViewById(com.movhaul.customer.R.id.textview_emergency_number2);
+        tv_emergency_relation1 = (TextView) findViewById(com.movhaul.customer.R.id.textview_emergency_relation1);
+        tv_emergency_relation2 = (TextView) findViewById(com.movhaul.customer.R.id.textview_emergency_relation2);
+
+
+
+
+        lt_edit = (LinearLayout) findViewById(com.movhaul.customer.R.id.linear_edit);
+        lt_show = (LinearLayout) findViewById(com.movhaul.customer.R.id.linear_show);
+
+        btn_back = (com.rey.material.widget.LinearLayout) findViewById(com.movhaul.customer.R.id.layout_back);
+
+        til_name = (TextInputLayout) findViewById(com.movhaul.customer.R.id.til_name);
+        til_phone = (TextInputLayout) findViewById(com.movhaul.customer.R.id.til_phone);
+        til_relation = (TextInputLayout) findViewById(com.movhaul.customer.R.id.til_relation);
+
+        til_name.setTypeface(tf);
+        til_phone.setTypeface(tf);
+        til_relation.setTypeface(tf);
+
+        tv_hint.setVisibility(View.VISIBLE);
+        lt_edit.setVisibility(View.GONE);
+        lt_show.setVisibility(View.GONE);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(EmergencyContacts.this);
+
+        service_token = sharedPreferences.getString("token","");
+        service_id = sharedPreferences.getString("id","");
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(EmergencyContacts.this, DashboardNavigation.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                emergencyName1=tv_emergency_name1.getText().toString();
+                emergencyName2=tv_emergency_name2.getText().toString();
+                emergencyNumber1=tv_emergency_no1.getText().toString();
+                emergencyNumber2=tv_emergency_no2.getText().toString();
+                emergencyRelation1=tv_emergency_relation1.getText().toString();
+                emergencyRelation2=tv_emergency_relation2.getText().toString();
+
+                if(tv_hint.getVisibility() == View.VISIBLE){
+                    Log.e("tag","0"+emergencyRelation1+emergencyName1+emergencyNumber1);
+
+                    tv_hint.setVisibility(View.GONE);
+                    lt_edit.setVisibility(View.VISIBLE);
+                    lt_show.setVisibility(View.GONE);
+                    new emergency_contacts().execute();
+                }
+                else if(lt_edit.getVisibility() == View.VISIBLE){
+                    Log.e("tag","1");
+                    tv_hint.setVisibility(View.GONE);
+                    lt_edit.setVisibility(View.GONE);
+                    lt_show.setVisibility(View.VISIBLE);
+                }
+                else if(lt_show.getVisibility() == View.VISIBLE){
+                    Log.e("tag","2");
+                    tv_hint.setVisibility(View.GONE);
+                    lt_edit.setVisibility(View.VISIBLE);
+                    lt_show.setVisibility(View.GONE);
+                }
+
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i = new Intent(EmergencyContacts.this,DashboardNavigation.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+
+    public class emergency_contacts extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.e("tag", "reg_preexe");
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String json = "", jsonStr = "";
+
+            try {
+                JSONObject jsonObject = new JSONObject();
+
+                //{"emergency_name":"test1","emergency_mobile":"+91668736826",
+               // "emergency_relation":"brother"}
+                jsonObject.accumulate("emergency_name", emergencyName1);
+                jsonObject.accumulate("emergency_mobile",  emergencyNumber1);
+                jsonObject.accumulate("emergency_relation", emergencyRelation1);
+
+                json = jsonObject.toString();
+                Log.e("tag","<----service_token--->"+service_token);
+                return jsonStr = HttpUtils.makeRequest1(Config.WEB_URL + "customer/insertemergency", json,service_id,service_token);
+
+            } catch (Exception e) {
+                Log.e("InputStream", e.getLocalizedMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.e("tag", "tag" + s);
+
+
+
+
+
+        }
+
+    }
+
+}
