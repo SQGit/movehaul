a738eeb428ba18e38168bfad99824c1a487cc687
mbrsalman5 <salman@sqindia.net>
2017-08-02 18:31:55 +0530

    document started

--- app/src/main/java/com/movhaul/customer/Book_now.java
+++ app/src/main/java/com/movhaul/customer/Book_now.java
@@ -45,6 +45,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
@@ -716,7 +717,8 @@ public class Book_now extends Activity {
                             entity.addPart("bookinggoods", new FileBody(new File(images.toString()), "image/jpeg"));
 
 
-                        httppost.setEntity(entity);
+                      httppost.setEntity(entity);
+
 
                         try {
                             response = httpclient.execute(httppost);
